7ba32a83064e9bdebdbae66cad4ff80b9deac9f6
unknown <ramonarents@hotmail.com>
2024-05-10 22:53:49 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const mineflayer = require("mineflayer");
+const { pathfinder, Movements, goals } = require("mineflayer-pathfinder");
+const GoalFollow = goals.GoalFollow;
+
+/* Uitproberen op versie van mindgek onder 1.20.5 */
+const bot = mineflayer.createBot({
+  host: "25.59.45.64",
+  port: 25565,
+  username: "chip",
+});
+
+bot.loadPlugin(pathfinder);
+
+function followPlayer() {
+  const playerCI = bot.players["DarthMasterRa"];
+
+  if (!playerCI) {
+    bot.chat("I can't see CI!");
+    return;
+  }
+
+  const mcData = require("minecraft-data")(bot.version);
+  const movements = new Movements(bot, mcData);
+  bot.pathfinder.setMovements(movements);
+
+  const goal = new GoalFollow(playerCI.entity, 1);
+  bot.pathfinder.setGoal(goal, true);
+}
+
+//Looks at nearest player. Not in use now.
+// function lookAtNearesPlayer() {
+//   const playerFilter = (entity) => entity.type === "player";
+//   const playerEntity = bot.nearestEntity(playerFilter);
+
+//   if (!playerEntity) return;
+
+//   const pos = playerEntity.position.offset(0, playerEntity.height, 0);
+//   bot.lookAt(pos);
+// }
+
+//once runs only one time
+bot.once("spawn", followPlayer);
+
+//Error handlling
+//on runs always
+bot.on("error", (err) => {
+  console.error("Error: ", err);
+});
+
+bot.on("end", () => {
+  console.log("Bot disconnected.");
+});
+
+//Bot stays looking at you
+//bot.on('physicTick', lookAtNearesPlayer);
