d7aa04052cf231352eb0e9a455f28db574816462
unknown <ramonarents@hotmail.com>
2024-05-13 18:14:27 +0200

    Find emerald block function and follow player again after 5 sec

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mineflayer = require("mineflayer");
 const { pathfinder, Movements, goals } = require("mineflayer-pathfinder");
 const GoalFollow = goals.GoalFollow;
+const GoalBlock = goals.GoalBlock;
 
 /* Uitproberen op versie van mindgek onder 1.20.5 */
 const bot = mineflayer.createBot({
@@ -10,6 +11,9 @@ const bot = mineflayer.createBot({
 });
 
 bot.loadPlugin(pathfinder);
+
+let isFollowing = false;
+let emeraldBlockFound = false;
 
 function followPlayer() {
   const playerCI = bot.players["DarthMasterRa"];
@@ -26,7 +30,6 @@ function followPlayer() {
 
   const goal = new GoalFollow(playerCI.entity, 1);
   bot.pathfinder.setGoal(goal, true);
-
 }
 
 function locateEmeraldBlocks() {
@@ -34,9 +37,35 @@ function locateEmeraldBlocks() {
 
   const movements = new Movements(bot, mcData);
   movements.scafoldingBlocks = [];
-  
+
   bot.pathfinder.setMovements(movements);
 
+  const emeraldBlock = bot.findBlock({
+    matching: mcData.blocksByName.emerald_block.id,
+    maxDistance: 32,
+  });
+
+  if (!emeraldBlock) {
+    bot.chat("I can't see emerald block. Going to follow player.");
+
+  }
+
+  const x = emeraldBlock.position.x;
+  const y = emeraldBlock.position.y + 1;
+  const z = emeraldBlock.position.z;
+  const goal = new GoalBlock(x, y, z);
+  bot.pathfinder.setGoal(goal);
+  
+}
+
+function handleTimeout(){
+  if (emeraldBlockFound) {
+    emeraldBlockFound = false;
+    setTimeout(() => {
+      isFollowing = true;
+      followPlayer();
+    }, 5000);
+  }
 }
 
 //Looks at nearest player. Not in use now.
@@ -51,7 +80,25 @@ function locateEmeraldBlocks() {
 // }
 
 //once runs only one time
-bot.once("spawn", followPlayer);
+bot.once("spawn", () =>{
+  //This code follows the player until an (placed) emerald block is found. After that it will stay 5 sec on the emerald block. After that he will follow the player again.
+  isFollowing = true;
+  followPlayer();
+
+  bot.on('blockUpdate', (oldBlock, newBlock) => {
+    if (newBlock.name === 'emerald_block') {
+      emeraldBlockFound = true;
+      if (isFollowing) {
+        isFollowing = false;
+        locateEmeraldBlocks();
+      }
+    }
+  });
+
+  bot.on('goal_reached', () => {
+    handleTimeout();
+  });
+});
 
 //Error handlling
 //on runs always
